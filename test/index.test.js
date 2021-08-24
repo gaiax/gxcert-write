089db36e1c6d0cade1c40781963201c8eb03c1f5
lucky <takuto.yoshikai@gmail.com>
2021-08-24 19:20:38 +0900

    test:

--- test/index.test.js
+++ test/index.test.js
@@ -5,42 +5,15 @@ const alice = web3.eth.accounts.create();
 const bob = web3.eth.accounts.create();
 const charlie = web3.eth.accounts.create();
 const dave = web3.eth.accounts.create();
-const contractAddress = "0xB16070e105567515FCA925eD621229DDc75815B7";
+const contractAddress = "0x2d0059214A0c5D4117F630Ab5C5cDf412Cc45f1e";
 const writer = new GxCertWriter(web3, contractAddress, charlie.privateKey.slice(2));
 const GxCertClient = require("gxcert-lib");
 const client = new GxCertClient(web3);
 const assert = require("assert");
-const expect = require("chai");
 
 let groupId;
 
 describe("GxCertWriter", () => {
-  describe("write", () => {
-    it("createCert", async () => {
-      await writer.init();
-      await client.init();
-      const certificate = {
-        context: {},
-        from: alice.address,
-        to: bob.address,
-        issued_at: (new Date()).getTime(),
-        title: "title",
-        description: "description",
-        image: "image",
-        url: "https://example.com",
-      }
-      const signedObject = await client.signCertificate(certificate, alice.privateKey);
-      try {
-        await writer.write(charlie.address, signedObject);
-      } catch(err) {
-        console.error(err);
-        assert.fail();
-        return;
-      }
-      assert.ok();
-    });
-  });
-
   describe("Group", () => {
     it("create group", async () => {
       const group = {
@@ -48,13 +21,12 @@ describe("GxCertWriter", () => {
         member: alice.address,
       }
       try {
-        groupId = await writer.createGroup(charlie.address, group);
+        await writer.createGroup(charlie.address, group);
       } catch(err) {
         console.error(err);
         assert.fail();
         return;
       }
-      assert.equal(groupId >= 0, true);
     });
     it ("invite member to group by wrong sign", async () => {
       const signedAddress = await client.signMemberAddress(dave.address, bob.privateKey);
@@ -79,4 +51,31 @@ describe("GxCertWriter", () => {
       }
     });
   });
+  describe("write", () => {
+    it("createCert", async () => {
+      await writer.init();
+      await client.init();
+      const certificate = {
+        context: {},
+        from: alice.address,
+        to: bob.address,
+        issued_at: (new Date()).getTime(),
+        title: "title",
+        description: "description",
+        image: "image",
+        url: "https://example.com",
+        groupId,
+      }
+      const signedObject = await client.signCertificate(certificate, alice.privateKey);
+      try {
+        await writer.write(charlie.address, signedObject);
+      } catch(err) {
+        console.error(err);
+        assert.fail();
+        return;
+      }
+      assert.ok();
+    });
+  });
+
 });
